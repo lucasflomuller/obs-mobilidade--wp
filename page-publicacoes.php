72ec3f7f38726c5780f38045429123c0ac71acda
luizmuraro <luizmuraro@gmail.com>
2019-02-18 03:06:08 -0300

    Templates adicionados

---
+++ page-publicacoes.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+
+<header class="publicacoes-header">
+    <div class="row">
+      <div class="col-2-of-3">
+        <h1 class="heading-secondary u-margin-bottom-small">
+          Publicações
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ab, ut quas quaerat optio voluptate rerum
+          maiores quasi laboriosam consequuntur, enim porro dolorum, voluptas laudantium tempore? Esse hic soluta
+          repellat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, expedita hic soluta.
+        </p>
+      </div>
+    </div>
+  </header>
+
+  <section class="section-categories">
+
+    <div class="row gutter-xsmall">
+      <div class="news-card news-card--1 u-padding-medium">
+        <a href="#" class="u-no-decoration">
+          <h4 class="heading-quartiary u-text-orange u-margin-bottom-small">
+            Projetos
+          </h4>
+        </a>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptatum quaerat nam aliquam dolorem earum
+          tempore iste quia porro distinctio fuga dicta cumque, pariatur perferendis?
+        </p>
+      </div>
+    </div>
+
+    <div class="row gutter-xsmall">
+      <div class="news-card news-card--2 u-padding-medium">
+        <a href="subpages/publicacoes/downloads.html" class="u-no-decoration">
+          <h4 class="heading-quartiary u-text-primary-light u-margin-bottom-small">
+            Publicações
+          </h4>
+        </a>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptatum quaerat nam aliquam dolorem earum
+          tempore iste quia porro distinctio fuga dicta cumque, pariatur perferendis?
+        </p>
+      </div>
+    </div>
+
+    <div class="row gutter-xsmall">
+      <div class="news-card news-card--4 u-padding-medium">
+        <a href="#" class="u-no-decoration">
+          <h4 class="heading-quartiary u-text-green u-margin-bottom-small">
+            Clipping
+          </h4>
+        </a>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptatum quaerat nam aliquam dolorem earum
+          tempore iste quia porro distinctio fuga dicta cumque, pariatur perferendis?
+        </p>
+      </div>
+    </div>
+
+  </section>
+
+<?php get_footer(); ?>
