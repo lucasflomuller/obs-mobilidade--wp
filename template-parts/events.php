489b323bb9206bf4e07cba215becb68ecda9d664
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-02-20 15:34:09 -0300

    Add files via upload

--- template-parts/events.php
+++ template-parts/events.php
@@ -1,41 +1,3 @@
-<?php get_header() ?>
-
-  <header class="home-header">
-    <div class="row u-margin-bottom-zero-tab-port">
-      <div class="col-2-of-3">
-        <h1 class="heading-secondary u-margin-bottom-small">
-          Observatório de Mobilidade Urbana UFSC
-        </h1>
-        <p class="u-margin-bottom-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Perspiciatis, esse doloribus! Quidem saepe numquam suscipit
-          recusandae repudiandae corrupti rem itaque consequuntur obcaecati
-          harum quaerat, officia assumenda a non voluptatum deleniti nobis id
-          hic error sit aperiam. Saepe distinctio illum, voluptatum
-          necessitatibus nostrum esse consequuntur suscipit earum quae
-          corporis cumque nihil.
-        </p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-1-of-3">
-        <p class="lead-text--secondary">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-          voluptatibus quam numquam. Optio perspiciatis reiciendis natus
-          fugiat consequatur numquam.
-        </p>
-        <a href="#" class="btn btn--primary-outline">Saiba mais sobre o observatório</a>
-      </div>
-      <div class="col-1-of-3">
-        <p class="lead-text--secondary">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-          voluptatibus quam numquam. Optio perspiciatis reiciendis natus
-          fugiat consequatur numquam.
-        </p>
-        <a href="#" class="btn btn--primary-outline">Conheça nossa biblioteca</a>
-      </div>
-    </div>
-  </header>
   <section class="section-events">
       <div class="row">
         <div class="events">
@@ -116,29 +78,3 @@
         </div>
       </div>
     </section>
-    <main class="section-main" role="main">
-      <?php get_template_part( 'template-parts/events' ); ?>  
-      <?php get_template_part( 'template-parts/news' ); ?>  
-    </main>
-
-  <section class="section-more">
-    <div class="row">
-      <div class="col-2-of-3 u-margin-bottom-zero-tab-port">
-        <h3 class="heading-tertiary u-margin-bottom-xbig">
-          Saiba mais sobre o projeto de Transporte Integrado Metropolitano
-        </h3>
-        <p class="lead-text--primary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur similique mollitia excepturi
-          molestias magnam dolores nemo in. Debitis, dolor!
-        </p>
-        <a href="#" class="btn btn--primary-outline">
-          Veja os mapas do novo sistema
-        </a>
-      </div>
-      <div class="col-1-of-3">
-        <img class="section-more__image" src="<?php echo get_template_directory_uri() . '/img/preview-mapas.png' ?>">
-      </div>
-    </div>
-  </section>
-
-<?php get_footer() ?>
---
+++ template-parts/news.php
@@ -0,0 +1,89 @@
+  <div class="section-news">
+      <div class="row">
+        <div class="col-2-of-3">
+          <h3 class="heading-tertiary u-text-white u-margin-bottom-small">
+            Acompanhe nossas últimas notícias
+          </h3>
+          <p class="lead-text--primary u-text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rem
+            excepturi cumque a, deserunt distinctio laborum placeat nam earum
+            at, quisquam eos!
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="news-card__carousel">
+          <i class="fas fa-angle-left"></i>
+          <div class="row">
+            <div class="col-1-of-3">
+              <div class="news-card news-card--1">
+                <a href="./subpages/noticias.html" class="u-no-decoration">
+                  <h4 class="heading-quartiary u-text-orange">
+                    Excepteur sint occaeat cupididate non proident sunt in
+                    culpa.
+                  </h4>
+                </a>
+                <p class="lead-text--secondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quia, accusantium a doloremque consectetur consequuntur cum
+                  veniam libero animi explicabo et eaque.
+                </p>
+                <p class="news-card__footer lead-text--secondary u-text-orange">
+                  <span>
+                    <small>
+                      <i class="fas fa-clock"></i> 09/01/2018 - 14:35
+                    </small>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div class="col-1-of-3">
+              <div class="news-card news-card--2">
+                <a href="./subpages/noticias.html" class="u-no-decoration">
+                  <h4 class="heading-quartiary u-text-primary-light">
+                    Excepteur sint occaeat cupididate non proident sunt in
+                    culpa.
+                  </h4>
+                </a>
+                <p class="lead-text--secondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quia, accusantium a doloremque consectetur consequuntur cum
+                  veniam libero animi explicabo et eaque.
+                </p>
+                <p class="news-card__footer lead-text--secondary u-text-primary-light">
+                  <span>
+                    <small>
+                      <i class="fas fa-clock"></i> 09/01/2018 - 14:35
+                    </small>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div class="col-1-of-3">
+              <div class="news-card news-card--3">
+                <a href="./subpages/noticias.html" class="u-no-decoration">
+                  <h4 class="heading-quartiary u-text-purple">
+                    Excepteur sint occaeat cupididate non proident sunt in
+                    culpa.
+                  </h4>
+                </a>
+                <p class="lead-text--secondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quia, accusantium a doloremque consectetur consequuntur cum
+                  veniam libero animi explicabo et eaque.
+                </p>
+                <p class="news-card__footer lead-text--secondary u-text-purple">
+                  <span>
+                    <small>
+                      <i class="fas fa-clock"></i> 09/01/2018 - 14:35
+                    </small>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <i class="fas fa-angle-right"></i>
+        </div>
+      </div>
+    </div>
