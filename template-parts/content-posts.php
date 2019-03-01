4cdb13d3bcaec918711e3164ee06d815ddd24b1f
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-02-28 22:06:11 -0300

    Add files via upload
    wp_query publicações

---
+++ template-parts/content-posts.php
@@ -0,0 +1,14 @@
+<div class="row gutter-xsmall padding-bottom">
+  <div class="news-card news-card--1 u-padding-medium">
+  <a href="#" class="u-no-decoration">
+    <h4 class="heading-quartiary u-text-primary-light u-margin-bottom-small">
+            <?php the_title(); ?>
+    </h4>
+  </a>
+  <p>
+    <?php echo get_the_excerpt(); ?>
+  </p>
+  <p class="news-card__footer lead-text--secondary u-text-orange">
+  </p>
+  </div>
+</div>
