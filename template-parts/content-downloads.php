3024f419351c32241fee6e8329b0c90c7db0c2ec
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-03-01 16:44:34 -0300

    Add files via upload
    apenas a estrutura da parte do single downloads

---
+++ template-parts/content-downloads.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<body class="body__downloads">
+  
+  <main class="main-downloads">
+    <div class="row">
+      <div class="downloads">
+        <header class="downloads__header u-margin-bottom-medium">
+          <h3 class="heading-quinary u-font-size-medium u-text-primary-light downloads__title">
+            APRESENTAÇÕES
+          </h3>
+          <span class="u-text-primary-light"><small><b>
+            VEJA TAMBÉM:
+          </b></small></span>
+          <a href="#" class="btn btn--primary-light-outline">
+            Relatórios
+          </a>
+          <a href="#" class="btn btn--primary-light-outline">
+            Microdados
+          </a>
+        </header>
+
+        <section class="downloads__content">
+          <ul class="downloads__list">
+            <li class="downloads__item">
+              <span class="icon downloads__icon"></span>
+              Apresentações de xx/xx/2017 a xx/xx/2018
+              <ul class="downloads__list downloads__list--nested">
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 01 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 02 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 03 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 04 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 05 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 06 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 07 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 08 - Lorem ipsum dolor sit amet
+                </li>
+              </ul>
+            </li>
+            <li class="downloads__item">
+              <span class="icon downloads__icon"></span>
+              Apresentações de xx/xx/2017 a xx/xx/2018
+              <ul class="downloads__list downloads__list--nested">
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 01 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 02 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 03 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 04 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 05 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 06 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 07 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 08 - Lorem ipsum dolor sit amet
+                </li>
+              </ul>
+            </li>
+            <li class="downloads__item">
+              <span class="icon downloads__icon"></span>
+              Apresentações de xx/xx/2017 a xx/xx/2018
+              <ul class="downloads__list downloads__list--nested">
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 01 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 02 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 03 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 04 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 05 - Lorem ipsum dolor sit amet
+                </li>
+                <li class="downloads__item downloads__item--nested">
+                  <span class="icon downloads__icon--nested"></span>
+                  Download 06 - Lorem ipsum dolor sit amet
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  </main>
+
+  <section class="section-relacionados">
+    <div class="row">
+      <img src="../../img/backgrounds/bg-8.svg" alt="Graph layout" class="section-relacionados__image">
+      <div class="relacionados">
+        <p class="relacionados__title">
+          Projetos relacionados:
+        </p>
+        <div class="relacionados__btn-list">
+          <a href="#" class="btn btn--orange-outline relacionados__btn">
+            Projeto neotrans
+          </a>
+          <a href="#" class="btn btn--green-outline relacionados__btn">
+            Publicações academicas
+          </a>
+          <a href="#" class="btn btn--purple-outline relacionados__btn">
+            Estudos anteriores
+          </a>
+          <a href="#" class="btn btn--blue-sea-outline relacionados__btn">
+            Manifestos
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</body>
+
+</html>
