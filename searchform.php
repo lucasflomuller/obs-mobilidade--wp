23fb559eb3875cdf0fe4760fb9e3adb91c7b0ba2
lucasflomuller <lucasflorianomuller@gmail.com>
2019-03-10 20:14:14 +0000

    start implement search-bar

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form class="navigation__form" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+  <div class="search-bar">
+    <input id="search-bar" type="text" placeholder="Pesquisar no site" class="search-bar__input">
+    <label for="search-bar" class="search-bar__label">Pesquisar no Site:</label>
+    <button class="search-bar__button">
+      <i class="fas fa-search"></i>
+    </button>
+  </div>
+</form>
