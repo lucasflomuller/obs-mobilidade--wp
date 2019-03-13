dba77be4e2d2c9f898073d3dc8732798d884b794
lucasflomuller <lucasflorianomuller@gmail.com>
2019-03-13 23:19:56 +0000

    add favicon

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Observatório da Mobilidade</title>
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(  ) . "/img/logo-menu.ico" ?>" />
   <?php wp_head() ?>
 </head>
 
